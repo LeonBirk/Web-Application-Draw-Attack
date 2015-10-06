a0e3605522de2c937e883a563f4eac17b3a5ce65
jannwickert <wickert.jann@gmail.com>
2015-10-06 16:00:22 +0200

    Es läuft!

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var alleSpieler = {};
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    app.use(express.static(__dirname + '/public'));
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function (spieler) {
