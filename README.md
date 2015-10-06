dda18f0acaf7c10cf4f7eff563147e0a3abaf21d
jannwickert <wickert.jann@gmail.com>
2015-10-06 16:56:48 +0200

    Spieler als Array

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Web-Application-Draw-Attack
-* Anmeldung
-    * Login mit Enter
+* TO DO
+        
+    * Spieler als Objekt (highest prio)
+        * Zustand
+        * Name
+        * usw.
+        
+    * Anmeldung
+        * Login mit Enter
+        * Alles schön (CSS, etc)
+    
+    * Chat
+        * schön
+        * box
+        * username checken (doppelter etc)
+        * Liniengedöhnse anpassen
+        * Name in extra Farbe + Fett
+        
+    * Leinwand
+        * alles
+        * regelung wer sendet, wer nicht
+        
+    * Rangliste / Userliste
+        * Zur Rangliste machen
+        * alles
+        * Wer ist da
+        * box
+    
--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var alleSpieler = {};
+/*
+mehrdimensionales Array, erste Dimension sind Spieler, zweite sind Attribute der Spieler
+1 = name
+2 = zustand (0 = rät, 1 = zeichnet)
+weitere beliebig hinzufügen und hier eintragen
+*/
+var alleSpieler = new Array();
 
 app.get('/', function (req, res) {
     app.use(express.static(__dirname + '/public'));
@@ -12,17 +18,24 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (spieler) {
     
-    
+        //BREAK nach gefunden fehlt
             spieler.on('chat message', function (msg) {
                 console.log(msg);
-                var nachricht = alleSpieler[spieler.id] + ": " + msg;
+                for (var i = 0; i < alleSpieler.length; i++) {
+                    if(alleSpieler[i].id == spieler.id) 
+                    var nachricht = alleSpieler[i].name + ": " + msg;
                 io.emit('chat message', nachricht);
+                }
+                
+                
             });
     
     
             //Anmeldung
             spieler.on('beitritt', function (name) {
-                alleSpieler[spieler.id] = name;
+                spieler.name = name;
+                alleSpieler.push(spieler);
+                console.log(spieler.id);
                 spieler.emit('beitritt', "Hallo du! Willkommen im Chat");
             } );
 });
