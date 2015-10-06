a89411c1e1bdb0d9ccccfd2decc98d3b5f864139
jannwickert <wickert.jann@gmail.com>
2015-10-06 15:10:54 +0200

    richtige dateien

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var alleSpieler = {};
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-      console.log(msg);
-    io.emit('chat message', msg);
-  });
+io.on('connection', function (spieler) {
+    
+    
+            spieler.on('chat message', function (msg) {
+                console.log(msg);
+                var nachricht = alleSpieler[spieler.id]
+                io.emit('chat message', msg);
+            });
+    
+    
+            //Anmeldung
+            spieler.on('beitritt', function (name) {
+                alleSpieler[spieler.id] = name;
+                spieler.emit('beitritt', "Hallo du! Willkommen im Chat");
+            } );
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
+
+        http.listen(3000, function () {
+            console.log('listening on *:3000');
+        });
