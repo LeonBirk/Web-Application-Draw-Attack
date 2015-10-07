0ca704b06845ee805951a42dcc28a945eec08392
LeonBirk <leon.birk@outlook.com>
2015-10-07 09:36:16 +0200

    beautify

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (spieler) {
-    
-        //BREAK nach gefunden fehlt
-            spieler.on('chat message', function (msg) {
-                console.log(msg);
-                for (var i = 0; i < alleSpieler.length; i++) {
-                    if(alleSpieler[i].id == spieler.id) 
-                    var nachricht = alleSpieler[i].name + ": " + msg;
-                io.emit('chat message', nachricht);
-                }
-                
-                
-            });
-    
-    
-            //Anmeldung
-            spieler.on('beitritt', function (name) {
-                spieler.name = name;
-                alleSpieler.push(spieler);
-                console.log(spieler.id);
-                spieler.emit('beitritt', "Hallo du! Willkommen im Chat");
-            } );
+
+    //BREAK nach gefunden fehlt
+    spieler.on('chat message', function (msg) {
+        console.log(msg);
+        for (var i = 0; i < alleSpieler.length; i++) {
+            if (alleSpieler[i].id == spieler.id)
+                var nachricht = alleSpieler[i].name + ": " + msg;
+            io.emit('chat message', nachricht);
+        }
+
+
+    });
+
+
+    //Anmeldung
+    spieler.on('beitritt', function (name) {
+        spieler.name = name;
+        alleSpieler.push(spieler);
+        console.log(spieler.id);
+        spieler.emit('beitritt', "Hallo du! Willkommen im Chat");
+    });
 });
 
 
 
-        http.listen(3000, function () {
-            console.log('listening on *:3000');
-        });
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
